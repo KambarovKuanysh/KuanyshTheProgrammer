fc36b35fcf542ec307cf1146bad7b3afb80d18cb
KambarovKuanysh <k.kambarov@a1qa.com>
2023-04-19 16:35:42 +0600

    Method added: get

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -64,7 +64,13 @@ public class MyLinkedList<E> implements MyList{
 
     @Override
     public Object get(int index) {
-        return null;
+        Node toSearch;
+        toSearch = this.head;
+        while (index != 0) {
+            toSearch = toSearch.next;
+            index--;
+        }
+        return (E) toSearch.val;
     }
 
     @Override
