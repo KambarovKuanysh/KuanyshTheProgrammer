0bed99b1aca56dec691c898a4f833552b35911b7
KambarovKuanysh <k.kambarov@a1qa.com>
2023-04-19 09:14:51 +0600

    Method added: add

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,3 +1,22 @@
-public class MyArrayList <T>{
+public class MyArrayList <T> {
+    private T[] arr;
+    private int size;
 
+    MyArrayList(){
+        this.arr = (T[]) new Object[5];
+        this.size = 0;
+    }
+    public void add(T item){
+        if(size == arr.length){
+            increaseBuffer();
+        }
+        arr[size++] = item;
+    }
+    public void increaseBuffer(){
+        T[] newArr = (T[]) new Object[arr.length*2];
+        for(int i=0; i< arr.length; i++){
+            newArr[i]=arr[i];
+        }
+        arr = newArr;
+    }
 }
