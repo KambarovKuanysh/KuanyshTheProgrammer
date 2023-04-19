2bbf2c02419345e71d490d6a89b1d1d354e5902b
KambarovKuanysh <k.kambarov@a1qa.com>
2023-04-19 20:41:32 +0600

    Showcase added

--- src/Main.java
+++ src/Main.java
@@ -2,51 +2,6 @@ import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Main {
-    public static void showcaseArrayList(){
-        Scanner sc = new Scanner(System.in);
-        MyArrayList myArrayListInt = new MyArrayList<Integer>();
-        System.out.println("Велком! Начнем с MyArrayList<Integer>:");
-        System.out.println("> Size: " + myArrayListInt.size());
-        System.out.println("Давайте добавим элементов типа <Integer>:");
-        System.out.println("Введите -1 чтобы остановить ввод");
-        int nextInt;
-        while(true) {
-            nextInt = sc.nextInt();
-            if (nextInt == -1) break;
-            myArrayListInt.add(nextInt);
-        }
-        System.out.println("> Your array: " + myArrayListInt);
-        System.out.println("> Size: " + myArrayListInt.size());
-
-        System.out.println("Давайте добавим элемент на индекс. Введите элемент и индекс через пробел:");
-        nextInt = sc.nextInt();
-        int nextIndex = sc.nextInt();
-        myArrayListInt.add(nextInt, nextIndex);
-
-        System.out.println("Давайте удалим элемент с вашим индексом. Введите его:");
-        nextIndex = sc.nextInt();
-        myArrayListInt.remove(nextIndex);
-        System.out.println("> Your array: " + myArrayListInt);
-
-        System.out.println("А теперь удалим элемент не зная его индекса. Введите число для удаления:");
-        nextInt = sc.nextInt();
-        myArrayListInt.remove((Object) nextInt);
-        System.out.println("> Your array: " + myArrayListInt);
-
-        System.out.println("А теперь давайте узнаем, находится ли введенный вами элемент в массиве?");
-        nextInt = sc.nextInt();
-        System.out.println(myArrayListInt.contains(nextInt));
-
-        System.out.println("А теперь давайте узнаем первый индекс вхождения введенного вами элемент в массиве");
-        nextInt = sc.nextInt();
-        System.out.println(myArrayListInt.indexOf(nextInt));
-
-        System.out.println("А теперь давайте узнаем последний индекс вхождения введенного вами элемент в массиве");
-        nextInt = sc.nextInt();
-        System.out.println(myArrayListInt.lastIndexOf(nextInt));
-    }
-
-
     public static void main(String[] args) {
         MyArrayList<Object> list = new MyArrayList<>();
         System.out.println("> Adding variables");
