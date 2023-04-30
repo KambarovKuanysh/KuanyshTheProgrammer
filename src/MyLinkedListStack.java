07fddd2bb1a5c33d1abc38cac8bf4ee13e563270
kfcboxmaster <kambarovkuanysh@gmail.com>
2023-04-30 19:31:44 +0600

    Documentation added: MyLinkedListStack

--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -1,28 +1,64 @@
 import java.util.EmptyStackException;
 
-public class MyLinkedListStack <E>{
+public class MyLinkedListStack<E> {
     private MyLinkedList<E> stack;
+
+    /**
+     * Constructs a new MyLinkedListStack object with an empty linked list.
+     */
     public MyLinkedListStack() {
         stack = new MyLinkedList<>();
     }
+
+    /**
+     * Pushes the specified element onto the top of this stack.
+     *
+     * @param element the element to push onto this stack
+     */
     public void push(E element) {
         stack.addFirst(element);
     }
-    public boolean isEmpty(){
+
+    /**
+     * Tests if this stack is empty.
+     *
+     * @return true if and only if this stack contains no items; false otherwise
+     */
+    public boolean isEmpty() {
         return stack.isEmpty();
     }
+
+    /**
+     * Removes the element at the top of this stack and returns that element.
+     *
+     * @return the element at the top of this stack
+     * @throws EmptyStackException if this stack is empty
+     */
     public E pop() {
         if (isEmpty()) {
             throw new EmptyStackException();
         }
         return stack.removeFirst();
     }
+
+    /**
+     * Looks at the element at the top of this stack without removing it from the stack.
+     *
+     * @return the element at the top of this stack
+     * @throws EmptyStackException if this stack is empty
+     */
     public E peek() {
         if (isEmpty()) {
             throw new EmptyStackException();
         }
         return stack.getFirst();
     }
+
+    /**
+     * Returns the number of elements in this stack.
+     *
+     * @return the number of elements in this stack
+     */
     public int size() {
         return stack.size();
     }
