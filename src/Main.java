fbbee89bed0224ca1ddcdb1c2a11fe5dd6579791
kfcboxmaster <kambarovkuanysh@gmail.com>
2023-04-30 13:36:31 +0600

    MyLinkedListStack: completed with tests

--- src/Main.java
+++ src/Main.java
@@ -28,8 +28,10 @@ public class Main {
         stack.push(2);
         System.out.println(stack.peek());
         System.out.println(stack.pop());
+        System.out.println(stack.size());
         System.out.println(stack.pop());
-        stack.pop();
+        System.out.println(stack.size());
+        System.out.println(stack.isEmpty());
 
     }
 }
